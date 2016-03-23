fa6cabf62977ed2faea045bb30be76cf8c454953
Fabien BERQUEZ <fabienfd.berquez@gmail.com>
2016-03-23 18:19:41 -0400

    Gestion de la recherche de places par preferences

---
+++ TP2/TP2final/app/src/main/java/com/example/alexmao/tp2final/IObserver.java
@@ -0,0 +1,10 @@
+package com.example.alexmao.tp2final;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Fabien on 23/03/2016.
+ */
+public interface IObserver {
+    public void update(ArrayList<Place> listPlaces);
+}
