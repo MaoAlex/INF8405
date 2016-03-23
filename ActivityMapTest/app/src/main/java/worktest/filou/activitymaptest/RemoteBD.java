572a6d65d3844e9a4ce885a01cd5f17cd22a0a25
Troclet Philippe <philippe.troclet@polymtl.ca>
2016-03-23 16:45:23 -0400

    Corrige RemoteBD.

--- ActivityMapTest/app/src/main/java/worktest/filou/activitymaptest/RemoteBD.java
+++ ActivityMapTest/app/src/main/java/worktest/filou/activitymaptest/RemoteBD.java
@@ -17,7 +17,6 @@ public interface RemoteBD {
     void getGroup(String groupID, MyGroup myGroup);
     void getGroupFromName(String name, MyGroup myGroup);
     void listenToChangeOnGroup(final MyGroup group, final String groupBDID);
-    void addUserPref(String id, List<String> pref);
     void addMdpToUser(String mail, String mdp);
     void getMdp(String mail, MdpWrapper mdpWrapper);
     void getExistGroup(String name, ExistWrapper existWrapper);
@@ -30,8 +29,17 @@ public interface RemoteBD {
     void listenToPlaceRespond(String userID);
     void listenToAvailabilities(String userID);
     void addMeeting(MeetingFinalChoice meetingFinalChoice, String groupID);
-    void addPlaceProposal(MeetingPlace meetingPlace, String groupID);
-    void addTimeProposal(TimeSlot timeSlot, String groupID);
     void setPlaceChoice(int index, String userID);
     void setTimeChoice(int index, String userID);
-}
+    void getUserPref(String id, LocalUserPreferences preferences);
+    void changeMail(LocalUser localUser, String newMail);
+    void update(LocalUser localUser, MyLocalGroup myLocalGroup,
+                       LocalUserPreferences localUserPreferences,
+                       OnUpdateComplete onUpdateComplete);
+    void addUserPref(String id, UserPreferences pref);
+    void addTimeProposal(TimeSlots timeSlots, String groupID);
+    void addPlacesProposal(PlaceProposals meetingPlace, String groupID);
+    void getPlaceProposal(String groupID, final LocalPlaceProposals placeProposals);
+    void getTimeProposal(final LocalTimeSlots timeSlots, String groupID);
+    void changeGroupName(MyLocalGroup myLocalGroup, String newName);
+    }
