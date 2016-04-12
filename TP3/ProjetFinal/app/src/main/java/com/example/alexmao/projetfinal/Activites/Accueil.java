6c93f5dd1338f158e3f0e7bc98ccc9210cf5e8ce
mao.alex <mao.a@outlook.fr>
2016-04-12 11:29:39 -0400

    mise à jour de la page d'accueil et bdd interne

--- TP3/ProjetFinal/app/src/main/java/com/example/alexmao/projetfinal/Activites/Accueil.java
+++ TP3/ProjetFinal/app/src/main/java/com/example/alexmao/projetfinal/Activites/Accueil.java
@@ -1,13 +1,14 @@
 package com.example.alexmao.projetfinal.Activites;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import com.example.alexmao.projetfinal.BDDInterne.GroupeBDD;
 import com.example.alexmao.projetfinal.BDDInterne.UtilisateurBDD;
 import com.example.alexmao.projetfinal.R;
 
-public class Accueil extends Activity {
+public class Accueil extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +18,40 @@ public class Accueil extends Activity {
         utilisateurBDD.open();
         GroupeBDD groupeBDD = new GroupeBDD(this);
         groupeBDD.open();
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
     }
 
+    //méthode pour la création du menu, dans notre cas les éléments de la tab bar
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.menu, menu);
+//        return true;
+//    }
+//
+//    //méthode permettant la gestion des actions en fonctions des boutons
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        switch (item.getItemId()) {
+//            case R.id.menu_about:
+//                // Comportement du bouton "A Propos"
+//                return true;
+//            case R.id.menu_help:
+//                // Comportement du bouton "Aide"
+//                return true;
+//            case R.id.menu_refresh:
+//                // Comportement du bouton "Rafraichir"
+//                return true;
+//            case R.id.menu_search:
+//                // Comportement du bouton "Recherche"
+//                return true;
+//            case R.id.menu_settings:
+//                // Comportement du bouton "Paramètres"
+//                return true;
+//            default:
+//                return super.onOptionsItemSelected(item);
+//        }
+//    }
 
 }
