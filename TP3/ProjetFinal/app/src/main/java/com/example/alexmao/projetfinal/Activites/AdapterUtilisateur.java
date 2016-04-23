256a3e355397e19672b82861a9b3f333e141786a
mao.alex <mao.a@outlook.fr>
2016-04-23 14:33:04 -0400

    Mise à jour de l'activité d'accueil, création de l'activité pour la tabBar, création de l'activité permettant l'affichage des membres
    Ajout sur l'activité d'accueil d'une première version de l'affichage des
    événements sous la forme de card List.
    Ajout d'une activité permettant le switch avec la tabBar.
    Ajout d'une activité permettant de voir les participants d'un
    groupe/événement sous la forme d'une card list

---
+++ TP3/ProjetFinal/app/src/main/java/com/example/alexmao/projetfinal/Activites/AdapterUtilisateur.java
@@ -0,0 +1,93 @@
+package com.example.alexmao.projetfinal.Activites;
+
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.alexmao.projetfinal.R;
+import com.example.alexmao.projetfinal.classeApp.Utilisateur;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.List;
+
+/**
+ */
+public class AdapterUtilisateur extends RecyclerView.Adapter<AdapterUtilisateur.UtilisateurViewHolder>{
+
+    private List<Utilisateur> persons;
+
+    AdapterUtilisateur(List<Utilisateur> persons){
+        this.persons = persons;
+        initializeData();
+    }
+
+
+
+    @Override
+    //Retourne le nombre d'éléments dans la liste
+    public int getItemCount() {
+        return persons.size();
+    }
+
+    @Override
+    public UtilisateurViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.card_view_membre, viewGroup, false);
+        UtilisateurViewHolder uvh = new UtilisateurViewHolder(v);
+        return uvh;
+    }
+
+    @Override
+    //Fonction permettant d'assigner les différents éléments avec leur valeur
+    public void onBindViewHolder(AdapterUtilisateur.UtilisateurViewHolder holder, int position) {
+        holder.personName.setText(persons.get(position).getNom());
+        holder.personAge.setText("test");
+        holder.personPhoto.setImageResource(R.drawable.user_chat1);
+    }
+
+
+
+    @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+    }
+
+    public static class UtilisateurViewHolder extends RecyclerView.ViewHolder {
+        CardView cv;
+        TextView personName;
+        TextView personAge;
+        ImageView personPhoto;
+
+        UtilisateurViewHolder(View itemView) {
+            super(itemView);
+            cv = (CardView)itemView.findViewById(R.id.cv);
+            personName = (TextView)itemView.findViewById(R.id.person_name);
+            personAge = (TextView)itemView.findViewById(R.id.person_age);
+            personPhoto = (ImageView)itemView.findViewById(R.id.person_photo);
+        }
+    }
+
+    private void initializeData(){
+        persons = new ArrayList<>();
+        Date date;
+        Utilisateur u1 = new Utilisateur();
+        u1.setNom("Jean Paul");
+        GregorianCalendar test = new GregorianCalendar(1993, 02, 24);
+        date = test.getTime();
+        u1.setDateNaissance(date);
+        Utilisateur u2 = new Utilisateur();
+        u2.setNom("Poly Technique");
+        u2.setDateNaissance(date);
+        Utilisateur u3 = new Utilisateur();
+        u3.setNom("Mont Real");
+        u3.setDateNaissance(date);
+        persons.add(u1);
+        persons.add(u2);
+        persons.add(u3);
+    }
+}
---
+++ TP3/ProjetFinal/app/src/main/java/com/example/alexmao/projetfinal/Activites/AffichageParticipant.java
@@ -0,0 +1,43 @@
+package com.example.alexmao.projetfinal.Activites;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.example.alexmao.projetfinal.R;
+import com.example.alexmao.projetfinal.classeApp.Utilisateur;
+
+import java.util.ArrayList;
+
+/**
+ * Classe permettant l'affichage des participants d'un événement, d'un groupe
+ */
+public class AffichageParticipant extends Activity {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_affichage_participant);
+        mRecyclerView = (RecyclerView) findViewById(R.id.rv);
+
+        //Permet l'optimisation des performances
+        // Dans le cas où l'on sait que la taille du layout RecyclerView ne va pas changer
+
+        mRecyclerView.setHasFixedSize(true);
+
+        // Utilisation d'un linear LayoutManager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // Spécification de l'adapter
+        ArrayList<Utilisateur> myDataset = new ArrayList<>();
+        mAdapter = new AdapterUtilisateur(myDataset);
+        mRecyclerView.setAdapter(mAdapter);
+
+    }
+
+}
