40b33024e4f2ac2f0565f75a3b694c78f37535ab
Troclet Philippe <philippe.troclet@polymtl.ca>
2016-04-24 00:15:20 -0400

    Rajoute un type d'invitation.

--- TP3/ProjetFinal/app/src/main/java/com/example/alexmao/projetfinal/BDDExterne/NotificationTypes.java
+++ TP3/ProjetFinal/app/src/main/java/com/example/alexmao/projetfinal/BDDExterne/NotificationTypes.java
@@ -6,6 +6,8 @@ package com.example.alexmao.projetfinal.BDDExterne;
 public abstract  class NotificationTypes {
     public static final String conctactInvitation = "conctactInvitation";
     public static final String eventInvitation = "eventInvitation";
+    public static final String groupInvitation = "groupInvitation";
     public static final String conctactInvitationAnswer = "conctactInvitationAnswer";
     public static final String eventInvitationAnswer = "eventInvitationAnswer";
+    public static final String groupInvitationAnswer = "groupInvitationAnswer";
 }
