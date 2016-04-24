624b77d204df2e4d33257dd81004e7cb5ec164b1
Fabien BERQUEZ <fabienfd.berquez@gmail.com>
2016-04-23 21:26:01 -0400

    Classes Parcelable Projet Final

--- TP3/ProjetFinal/app/src/main/java/com/example/alexmao/projetfinal/classeApp/Groupe.java
+++ TP3/ProjetFinal/app/src/main/java/com/example/alexmao/projetfinal/classeApp/Groupe.java
@@ -1,16 +1,27 @@
 package com.example.alexmao.projetfinal.classeApp;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.List;
 
 /**
  * Created by Fabien on 02/04/2016.
  */
-public class Groupe {
+public class Groupe implements Parcelable {
     private List<Utilisateur> listeMembre;
     private String idFirebase;
     private int idBDD;
     private Conversation conversation;
     private Evenement evenement;
+
+    public Groupe(Parcel in) {
+        listeMembre = in.readArrayList(Utilisateur.class.getClassLoader());
+        idFirebase = in.readString();
+        idBDD = in.readInt();
+        conversation = in.readParcelable(Conversation.class.getClassLoader());
+        evenement = in.readParcelable(Evenement.class.getClassLoader());
+    }
 
     public List<Utilisateur> getListeMembre() {
         return listeMembre;
@@ -51,4 +62,33 @@ public class Groupe {
     public void setEvenement(Evenement evenement) {
         this.evenement = evenement;
     }
+
+     /*
+      * Parcelable methods
+      */
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeList(listeMembre);
+        dest.writeString(idFirebase);
+        dest.writeInt(idBDD);
+        dest.writeParcelable(conversation, flags);
+        dest.writeParcelable(evenement, flags);
+    }
+
+    // Creator
+    public static final Parcelable.Creator<Groupe> CREATOR = new Parcelable.Creator<Groupe>() {
+        public Groupe createFromParcel(Parcel in) {
+            return new Groupe(in);
+        }
+
+        public Groupe[] newArray(int size) {
+            return new Groupe[size];
+        }
+    };
 }
--- TP3/ProjetFinal/app/src/main/java/com/example/alexmao/projetfinal/classeApp/InvitationConnexion.java
+++ TP3/ProjetFinal/app/src/main/java/com/example/alexmao/projetfinal/classeApp/InvitationConnexion.java
@@ -1,16 +1,27 @@
 package com.example.alexmao.projetfinal.classeApp;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.Date;
 
 /**
  * Created by Fabien on 02/04/2016.
  */
-public class InvitationConnexion {
+public class InvitationConnexion implements Parcelable {
     private Utilisateur expediteur;
     private Utilisateur invite;
     private Date date;
     private String idFirebase;
     private int idBDD;
+
+    public InvitationConnexion(Parcel in) {
+        expediteur = in.readParcelable(Utilisateur.class.getClassLoader());
+        invite = in.readParcelable(Utilisateur.class.getClassLoader());
+        date = (Date) in.readSerializable();
+        idFirebase = in.readString();
+        idBDD = in.readInt();
+    }
 
     public Utilisateur getExpediteur() {
         return expediteur;
@@ -51,4 +62,33 @@ public class InvitationConnexion {
     public void setIdBDD(int idBDD) {
         this.idBDD = idBDD;
     }
+
+    /*
+      * Parcelable methods
+      */
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeParcelable(expediteur, flags);
+        dest.writeParcelable(invite, flags);
+        dest.writeSerializable(date);
+        dest.writeString(idFirebase);
+        dest.writeInt(idBDD);
+    }
+
+    // Creator
+    public static final Parcelable.Creator<InvitationConnexion> CREATOR = new Parcelable.Creator<InvitationConnexion>() {
+        public InvitationConnexion createFromParcel(Parcel in) {
+            return new InvitationConnexion(in);
+        }
+
+        public InvitationConnexion[] newArray(int size) {
+            return new InvitationConnexion[size];
+        }
+    };
 }
